1ad5dbfb8b4b32aaa9050ff5324d223450315335
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-10-19 10:43:07 -0300

    Feat - Global Exception Handler

---
+++ src/main/java/dio/me/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package dio.me.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String>handleBusinessException(IllegalArgumentException businessException) {
+                                                                    //422
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
+                                                                    //400
+        return new ResponseEntity<>("Resource Id not found.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
+        String message = "Unexpected server error";
+        logger.error(message, unexpectedException);
+                                                //500
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
